5db46dbd1e167e1c44ac71a23c1929945a8dd05f
Dushan Terzikj <dusanterzic87@gmail.com>
2017-04-12 22:38:23 +0200

    Add Pet Content Provider (PetProvider.java) and include <provider> in the AndroidManifest.xml

---
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -0,0 +1,60 @@
+package com.example.android.pets.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by terz99 on 4/12/17.
+ */
+
+public class PetProvider extends ContentProvider{
+
+    // Log tag
+    private static final String LOG_TAG = PetProvider.class.getSimpleName();
+
+    // Database helper instance for linking the database to the pet content provider
+    private PetDbHelper mDbHelper;
+
+
+    /**
+     * This method instantiates the pet database helper
+     * @return true
+     */
+    @Override
+    public boolean onCreate() {
+        mDbHelper = new PetDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+}
